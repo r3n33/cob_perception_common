00e064de49c5f279d640035e26bec4f6a06907d9
r3n33 <renee1382@gmail.com>
2015-06-12 10:25:15 +0930

    Modified to work with PrimeSense

--- cob_image_flip/ros/src/image_flip.cpp
+++ cob_image_flip/ros/src/image_flip.cpp
@@ -295,72 +295,74 @@ void ImageFlip::imgDisconnectCB(const image_transport::SingleSubscriberPublisher
 template <typename T>
 void ImageFlip::pcCallback(const sensor_msgs::PointCloud2::ConstPtr& point_cloud_msg)
 {
-	ROS_WARN("Pointcloud rotation is currently not implemented.");
-//	// check camera link orientation and decide whether image must be turned around
-//	bool turnAround = false;
-//	tf::StampedTransform transform;
-//	try
-//	{
-//		transform_listener_->lookupTransform("/base_link", "/head_cam3d_link", ros::Time(0), transform);
-//		tfScalar roll, pitch, yaw;
-//		transform.getBasis().getRPY(roll, pitch, yaw, 1);
-//		if (roll > 0.0)
-//			turnAround = true;
-//		//      std::cout << "xyz: " << transform.getOrigin().getX() << " " << transform.getOrigin().getY() << " " << transform.getOrigin().getZ() << "\n";
-//		//      std::cout << "abcw: " << transform.getRotation().getX() << " " << transform.getRotation().getY() << " " << transform.getRotation().getZ() << " " << transform.getRotation().getW() << "\n";
-//		//      std::cout << "rpy: " << roll << " " << pitch << " " << yaw << "\n";
-//	} catch (tf::TransformException ex)
-//	{
-//		if (display_warnings_ == true)
-//			ROS_WARN("%s",ex.what());
-//	}
+	//ROS_WARN("Pointcloud rotation is currently not implemented.");
+	// check camera link orientation and decide whether image must be turned around
+	bool turnAround = false;
+	tf::StampedTransform transform;
+	try
+	{
+		//transform_listener_->lookupTransform("/base_link", "/camera_link", ros::Time(0), transform);
+		//tfScalar roll, pitch, yaw;
+		//transform.getBasis().getRPY(roll, pitch, yaw, 1);
+		//if (roll > 0.0)
+			//turnAround = true;
+		//      std::cout << "xyz: " << transform.getOrigin().getX() << " " << transform.getOrigin().getY() << " " << transform.getOrigin().getZ() << "\n";
+		//      std::cout << "abcw: " << transform.getRotation().getX() << " " << transform.getRotation().getY() << " " << transform.getRotation().getZ() << " " << transform.getRotation().getW() << "\n";
+		//      std::cout << "rpy: " << roll << " " << pitch << " " << yaw << "\n";
+	} catch (tf::TransformException ex)
+	{
+		if (display_warnings_ == true)
+			ROS_WARN("%s",ex.what());
+	}
+
+	if (turnAround == false)
+	{
+		// image upright
+		//sensor_msgs::Image color_image_turned_msg = *color_image_msg;
+		//color_image_turned_msg.header.stamp = ros::Time::now();
+		//color_camera_image_pub_.publish(color_image_turned_msg);
+		//sensor_msgs::PointCloud2::ConstPtr point_cloud_turned_msg = point_cloud_msg;
+		point_cloud_pub_.publish(point_cloud_msg);
+	}
+	else
+	{
+		// image upside down
+		// point cloud
+		pcl::PointCloud<T> point_cloud_src;
+		pcl::fromROSMsg(*point_cloud_msg, point_cloud_src);
 //
-//	if (turnAround == false)
-//	{
-//		// image upright
-//		//sensor_msgs::Image color_image_turned_msg = *color_image_msg;
-//		//color_image_turned_msg.header.stamp = ros::Time::now();
-//		//color_camera_image_pub_.publish(color_image_turned_msg);
-//		//sensor_msgs::PointCloud2::ConstPtr point_cloud_turned_msg = point_cloud_msg;
-//		point_cloud_pub_.publish(point_cloud_msg);
-//	}
-//	else
-//	{
-//		// image upside down
-//		// point cloud
-//		pcl::PointCloud<T> point_cloud_src;
-//		pcl::fromROSMsg(*point_cloud_msg, point_cloud_src);
-//
-//		boost::shared_ptr<pcl::PointCloud<T> > point_cloud_turned(new pcl::PointCloud<T>);
-//		//point_cloud_turned->header = point_cloud_msg->header;
-//		point_cloud_turned->header = pcl_conversions::toPCL(point_cloud_msg->header);
-//		point_cloud_turned->height = point_cloud_msg->height;
-//		point_cloud_turned->width = point_cloud_msg->width;
-//		//point_cloud_turned->sensor_orientation_ = point_cloud_msg->sensor_orientation_;
-//		//point_cloud_turned->sensor_origin_ = point_cloud_msg->sensor_origin_;
-//		point_cloud_turned->is_dense = true;	//point_cloud_msg->is_dense;
-//		point_cloud_turned->resize(point_cloud_src.height*point_cloud_src.width);
-//		for (int v = (int)point_cloud_src.height - 1; v >= 0; v--)
-//		{
-//			for (int u = (int)point_cloud_src.width - 1; u >= 0; u--)
-//			{
-//				(*point_cloud_turned)(point_cloud_src.width-1 - u, point_cloud_src.height-1 - v) = point_cloud_src(u, v);
-//			}
-//		}
-//
-//		// publish turned data
-//		sensor_msgs::PointCloud2::Ptr point_cloud_turned_msg(new sensor_msgs::PointCloud2);
-//		pcl::toROSMsg(*point_cloud_turned, *point_cloud_turned_msg);
-//		//point_cloud_turned_msg->header.stamp = ros::Time::now();
-//		point_cloud_pub_.publish(point_cloud_turned_msg);
-//
-//		//      cv::namedWindow("test");
-//		//      cv::imshow("test", color_image_turned);
-//		//      cv::waitKey(10);
-//	}
-//
-//	if (display_timing_ == true)
-//		ROS_INFO("%d ImageFlip: Time stamp of pointcloud message: %f. Delay: %f.", point_cloud_msg->header.seq, point_cloud_msg->header.stamp.toSec(), ros::Time::now().toSec()-point_cloud_msg->header.stamp.toSec());
+		boost::shared_ptr<pcl::PointCloud<T> > point_cloud_turned(new pcl::PointCloud<T>);
+		//point_cloud_turned->header = point_cloud_msg->header;
+		point_cloud_turned->header = pcl_conversions::toPCL(point_cloud_msg->header);
+		point_cloud_turned->height = point_cloud_msg->height;
+		point_cloud_turned->width = point_cloud_msg->width;
+		//point_cloud_turned->sensor_orientation_ = point_cloud_msg->sensor_orientation_;
+		//point_cloud_turned->sensor_origin_ = point_cloud_msg->sensor_origin_;
+		point_cloud_turned->is_dense = true;	//point_cloud_msg->is_dense;
+		point_cloud_turned->resize(point_cloud_src.height*point_cloud_src.width);
+		for (int v = (int)point_cloud_src.height - 1; v >= 0; v--)
+		{
+			for (int u = (int)point_cloud_src.width - 1; u >= 0; u--)
+			{
+				(*point_cloud_turned)(point_cloud_src.width-1 - u, point_cloud_src.height-1 - v) = point_cloud_src(u, v);
+			}
+		}
+
+		// publish turned data
+		sensor_msgs::PointCloud2::Ptr point_cloud_turned_msg(new sensor_msgs::PointCloud2);
+		pcl::toROSMsg(*point_cloud_turned, *point_cloud_turned_msg);
+		//point_cloud_turned_msg->header.stamp = ros::Time::now();
+		point_cloud_pub_.publish(point_cloud_turned_msg);
+
+		//      cv::namedWindow("test");
+		//      cv::imshow("test", color_image_turned);
+		//      cv::waitKey(10);
+	}
+
+	/* Diplayed even when flase in launch files?
+	if (display_timing_ == true)
+		ROS_INFO("%d ImageFlip: Time stamp of pointcloud message: %f. Delay: %f.", point_cloud_msg->header.seq, point_cloud_msg->header.stamp.toSec(), ros::Time::now().toSec()-point_cloud_msg->header.stamp.toSec());
+		*/
 }
 
 void ImageFlip::pcConnectCB(const ros::SingleSubscriberPublisher& pub)
